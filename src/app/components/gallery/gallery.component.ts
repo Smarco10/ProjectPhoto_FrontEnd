c08e84bad84f3ae32cd8b0070460b53c7c1f237c
Marc-Antoine MARTIN <mma.depannage@gmail.com>
2018-06-18 23:13:23 +0200

    updated gallery

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,32 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+
+@Component({
+    selector: 'app-gallery-elt',
+    template: '<ng-content *ngIf="!isHidden"></ng-content>',
+})
+export class GalleryElt {
+    isHidden: boolean = true;
+    constructor(){}
+}
 
 @Component({
     selector: 'app-gallery',
     templateUrl: './gallery.component.html',
     styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements AfterContentInit {
 
-    componentToExpose: any;
+@ContentChildren(GalleryElt)
+galleryElts: QueryList<GalleryElt>;
+    index: number = 0;
 
     constructor() { }
 
-    ngOnInit() {
+    ngAfterContentInit() {
+        console.log(galleryElts);
+        //show first elt if possible
     }
 
     private hasPreviousElt(): boolean {
-        return true; //TODO
+    return this.index > 0;
     }
 
     private previousElt(): void {
-        //TODO
+    if(hasPreviousElt()){
+    this.index--;
+    //TODO change visible elt and send externel event onPrevious
+    }
     }
 
     private hasNextElt(): boolean {
-        return true; //TODO
+    return this.index < galleryElts.length;
     }
 
     private nextElt(): void {
-        //TODO
+    if(hasNextElt()) {
+    this.index++;
+    //TODO change visible elt and send externel event onNext
+    }
     }
 }
