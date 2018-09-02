ece17adec5ed3361c840a9d7a35f3ed4dbcf3223
Marc-Antoine MARTIN <mma.depannage@gmail.com>
2018-09-02 23:02:02 +0200

    removed useless rest-client

--- src/app/models/identifiable.ts
+++ src/app/models/identifiable.ts
@@ -11,7 +11,7 @@ export class Identifiable {
     }
 
     public updateFromServer(serverData: any): boolean {
-        if (!this.id) {
+        if (!this.isCreated()) {
             this.id = serverData._id;
             return true;
         }
